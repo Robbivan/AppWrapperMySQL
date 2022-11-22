ff8df6aa0c6aab48c7e14f60129fa41e2378f553
Robbivan <96996213+robbivan@users.noreply.github.com>
2022-11-23 00:45:58 +0300

    Add try except finally

--- main.py
+++ main.py
@@ -2,13 +2,22 @@ import pymysql
 from config import host, user, password, db_name
 
 try:
+    connection = pymysql.connect(
+        host=host,
+        port=3306,
+        user=user,
+        password=password,
+        database=db_name,
+        cursorclass=pymysql.cursors.DictCursor
+    )
+    print("successfully connected")
+    try:
+        with connection.cursor() as cursor:
+            create_table_query =''
+    finally:
+        connection.close()
+
 except Exception as ex:
     print("Connection refused")
-connection = pymysql.connect(
-    host=host,
-    port=3306,
-    user=user,
-    password=password,
-    database=db_name,
-    cursorclass=pymysql.cursors.DictCursor
-)
+    print(ex)
+
